44e9527a78992c37faa5f628f90764b2f0e11b9d
boissonnfive <boissonnfive@gmail.com>
2015-03-31 12:04:36 +0200

    premier commit
    Affiche les accents au format ANSI

---
+++ ANSI.bat
@@ -0,0 +1,28 @@
+CLS REM On efface l'‚cran
+@echo off
+echo +-----------------------------------------------------------------------------+
+echo ^| Fichier     : ANSI.bat                                                      ^|
+echo ^| Version     : 0.1                                                           ^|
+echo ^| Auteur      : Bruno Boissonnet                                              ^|
+echo ^| Date        : 13/03/2015                                                    ^|
+echo ^| Description : Affiche les accents au format ANSI.                           ^|
+echo ^| Remarques   : Le fichier doit ˆtre sauvegard‚ au format ANSI.               ^|
+echo +-----------------------------------------------------------------------------+
+echo(
+echo Faire Ctrl+C pour quitter le programme ou appuyer sur une touche pour continuer.
+
+REM On fait une pause pour que la fenêtre reste affichée
+pause>nul
+
+REM +--DEBUT-DU-PROGRAMME---------------------------------------------------------+
+
+echo "Affichage des caractŠres diacritiques fran‡ais au format ANSI :"
+REM  "é è ç à â ê î ù"
+echo "‚ Š ‡ … ƒ ˆ Œ —"
+
+REM +--FIN-DU-PROGRAMME-----------------------------------------------------------+
+
+echo(
+REM On fait une pause pour que la fenêtre reste affichée
+echo Appuyer sur une touche pour quitter le programme.
+pause>nul 
